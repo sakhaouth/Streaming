7905dfa9bee25fdace70480d94be9caaa1c7907b
Nazmul Hasan <nazmul.buet17@gmail.com>
2022-08-17 01:08:25 +0600

    auth

--- app/src/main/java/com/example/cctvstreaming/HomePage.java
+++ app/src/main/java/com/example/cctvstreaming/HomePage.java
@@ -9,19 +9,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HomePage extends AppCompatActivity {
-    private Button streamButton;
+    private Button streamButton,logout;
     private ImageView imageView;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         streamButton = (Button) findViewById(R.id.stream_button);
         imageView = (ImageView) findViewById(R.id.imageView);
+        logout = findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Logging Out",Toast.LENGTH_LONG).show();
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),LogIn.class));
+            }
+        });
 
         streamButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/cctvstreaming/LogIn.java
+++ app/src/main/java/com/example/cctvstreaming/LogIn.java
@@ -1,37 +1,44 @@
 package com.example.cctvstreaming;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
     TextInputLayout emailTextView,passwordTextView;
     private TextView messageTexView,loginTextView;
-    private Button loginButton;
+    private Button loginButton,signupButton;
     private ProgressBar progressBar;
     private AlertDialog dialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         emailTextView = (TextInputLayout) findViewById(R.id.client_login_email);
         progressBar = (ProgressBar) findViewById(R.id.login_progress);
         passwordTextView = (TextInputLayout) findViewById(R.id.client_login_password);
         loginButton = (Button) findViewById(R.id.client_login_button);
         messageTexView = (TextView) findViewById(R.id.client_login_message_view);
-        loginTextView = (findViewById(R.id.client_login_text));
+        signupButton = findViewById(R.id.signUpButton);
+        mAuth = FirebaseAuth.getInstance();
 //        ArrayList<String> cameraNames = new ArrayList<>();
 //        cameraNames.add("Physics Lab");
 //        cameraNames.add("Bioa Lab");
@@ -39,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 //        DatabaseController.saveSchool(school);
 //        school = new School("Dr. Firoza Girls High School","Cumilla","Chandina",cameraNames);
 //        DatabaseController.saveSchool(school);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,22 +61,19 @@ public class MainActivity extends AppCompatActivity {
                 //email = emailTextView.getText().toString();
                 email = emailTextView.getEditText().getText().toString();
                 password = passwordTextView.getEditText().getText().toString();
-                if(email.compareTo("") == 0)
-                {
-                    messageTexView.setText("Type email");
-                    return;
+                if(TextUtils.isEmpty(email)) {
+                    emailTextView.setError("Please Provide Email");
+                    emailTextView.requestFocus();
+                }else if(TextUtils.isEmpty(password)){
+                    passwordTextView.setError("Please Type Password");
+                    passwordTextView.requestFocus();
+                }else {
+                    Log.d("pass",email+password);
+                    callDatabase(email,password);
                 }
-                if(password.compareTo("") == 0)
-                {
-                    messageTexView.setText("Type Password");
-                    return;
-                }
-                callDatabase(email,password);
+
 
             }
-
-
-
 
         });
 
@@ -70,19 +82,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void callDatabase(String email, String password) {
-        if(email.compareTo("shn065@gmail.com") == 0 && password.compareTo("noman065") == 0)
-        {
-
-            Intent intent = new Intent(getApplicationContext(),HomePage.class);
-            //intent.putExtra("uri",uri);
-            intent.putExtra("dis","Cumilla");
-            intent.putExtra("sub","Chandina");
-            startActivity(intent);
-        }
-        else
-        {
-            messageTexView.setText("LogIn error");
-        }
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(LogIn.this,"User Logged In Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),HomePage.class));
+                }else {
+                    Toast.makeText(LogIn.this,"User Logging Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
